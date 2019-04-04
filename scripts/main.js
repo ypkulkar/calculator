736735cc78833d96acaa56107dc448ed8b099483
Yashodhan Kulkarni <ypkulkarni@ucdavis.edu>
2019-04-04 10:54:24 -0700

    simple math functions

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// Basic math functions: add, subtract, multiply and divide
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a / b;
+
