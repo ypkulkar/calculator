5671d77c51dea7940cb8d9bb89db2f4549dfa0fb
Yashodhan Kulkarni <ypkulkarni@ucdavis.edu>
2019-04-04 23:21:11 -0700

    basic calculator skeleton

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,22 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 
+// Applies given operator on two numbers (a: add, s: subtract m: multiply, d: divide)
+function operate(a,b,o){
+    switch(o){
+        case "a":
+            return add(a,b);
+            break;
+        case "s":
+            return subtract(a,b);
+            break;
+        case "m":
+            return multiply(a,b);
+            break;
+        case "d":
+            return divide(a,b);
+            break;
+        default:
+            console.log("There is an error in operate()");
+    }
+}
