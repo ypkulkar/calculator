78c7f366e027ad21ca526ed6a9bb74b0203c093d
Yashodhan Kulkarni <ypkulkarni@ucdavis.edu>
2019-04-05 01:12:24 -0700

    changed text alignment

--- scripts/main.js
+++ scripts/main.js
@@ -39,7 +39,7 @@ allBtns.forEach(btn => {
         const display = document.querySelector("#display-text");
         if(btn.id === "clear-button"){
             display_text = "";
-            display.textContent= "TEXT";
+            display.textContent= "0";
         }
         else{
         display_text += text;
